32d1549974957600825e16f213138d3beda99569
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-15 17:54:41 +0200

    Completed bonusp2

--- src/org/lessons/java/valutazioni/Main.java
+++ src/org/lessons/java/valutazioni/Main.java
@@ -37,18 +37,60 @@ public class Main {
 		Studente[] promotedStudents = new Studente[nGraduated];
 		Studente[] rejectedStudents = new Studente[20-nGraduated];
 		
+		int counterP = 0;
+		int counterR = 0;
 		for (int i = 0; i < studenti.length; i++) {
+
+
 			if (studenti[i].isRejected()) {
-				int counter = 0;
-				rejectedStudents[counter] = studenti[i];
+				rejectedStudents[counterP] = studenti[i];
+				counterP++;
 			}
 			else {
-				int counter = 0;
-				promotedStudents[counter] = studenti[i];
+				promotedStudents[counterR] = studenti[i];
+				counterR++;
+
 			}
 		}
 		
+		//SOLO PROMOSSI STAMPA
+		System.out.println("\n--------------SOLO PROMOSSI");
+		//SOLO PROMOSSI
+		for (int i = 0; i < promotedStudents.length; i++) {
+			promotedStudents[i].printInfo();
+		}
 		
+		//SOLO BOCCIATI STAMPA
+				System.out.println("\n---------------------SOLO BOCCIATI");
+				//SOLO BOCCIATI
+				for (int i = 0; i < rejectedStudents.length; i++) {
+					rejectedStudents[i].printInfo();
+				}
+				
+				System.out.println();
+		
+			int lowestAvgID = 0;
+			double min = rejectedStudents[0].avgVotes;
+		for (int i = 1; i < rejectedStudents.length; i++) {
+			
+			if (rejectedStudents[i].avgVotes < min) {
+				lowestAvgID = rejectedStudents[i].id;
+			}
+			
+		}
+		
+		int highestAvgID = 0;
+		double max = promotedStudents[0].avgVotes;
+		
+		for (int i = 1; i < promotedStudents.length; i++) {
+			if (promotedStudents[i].avgVotes > max) {
+				highestAvgID = promotedStudents[i].id;
+			}
+		}
+		
+		System.out.println("Il bocciato coi voti più bassi ha ID " + lowestAvgID);
+		System.out.println("Il promosso coi voti più alti ha ID " + highestAvgID);
+
 	}
 
 }
