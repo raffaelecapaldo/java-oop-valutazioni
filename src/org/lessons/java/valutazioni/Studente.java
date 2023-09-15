03bb29e390535206b5ebed96274472165964d954
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-15 16:24:38 +0200

    Added isRejected logic

--- src/org/lessons/java/valutazioni/Studente.java
+++ src/org/lessons/java/valutazioni/Studente.java
@@ -2,4 +2,33 @@ package org.lessons.java.valutazioni;
 
 public class Studente {
 
+	int id;
+	int percAbsences;
+	double avgVotes;
+	
+	Studente(int id, int percAbsences, double avgVotes) {
+		this.id = id;
+		this.percAbsences = percAbsences;
+		this.avgVotes = avgVotes;
+	}
+	public boolean isRejected() {
+		boolean rejected = true;
+		
+		if (this.percAbsences > 50) {
+			return rejected;
+		}
+		else if (this.percAbsences >= 25 && this.percAbsences <= 50 && this.avgVotes >= 2) {
+			rejected = false;
+			return rejected;
+		}
+		else if (this.percAbsences < 25 && this.avgVotes >= 2) {
+			rejected = false;
+			return rejected;
+		}
+		
+		return rejected;
+
+		
+	}
+	
 }
