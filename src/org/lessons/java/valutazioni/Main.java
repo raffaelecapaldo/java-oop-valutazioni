7523675f6281dffb341bee6f66be1a0d6ffe9eac
raffaelecapaldo <raffaelecpl@gmail.com>
2023-09-15 15:51:50 +0200

    Initial commit

---
+++ src/org/lessons/java/valutazioni/Main.java
@@ -0,0 +1,10 @@
+package org.lessons.java.valutazioni;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
---
+++ src/org/lessons/java/valutazioni/Studente.java
@@ -0,0 +1,5 @@
+package org.lessons.java.valutazioni;
+
+public class Studente {
+
+}
